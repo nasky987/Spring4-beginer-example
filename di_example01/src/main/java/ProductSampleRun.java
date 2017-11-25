c9cc1514f7423a5d5f3efcf289983d50be3463cd
JOHN LEE <nasky987@gmail.com>
2017-11-25 18:24:35 +0900

    DI 기초 코드 작성, 원 소스와 다르게 xml설정 파일은 resources 밑으로 이동

--- di_example01/src/main/java/ProductSampleRun.java
+++ di_example01/src/main/java/ProductSampleRun.java
@@ -1,2 +1,23 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sample.di.business.domain.Product;
+import sample.di.business.service.ProductService;
+
 public class ProductSampleRun {
+    public static void main(String[] args) {
+        final ProductSampleRun productSampleRun = new ProductSampleRun();
+
+        productSampleRun.execute();
+    }
+
+    public void execute() {
+        final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/sample/config/applicationContext.xml");
+        final ProductService productService = applicationContext.getBean(ProductService.class);
+
+        productService.addProduct(new Product("공책", 100));
+
+        final Product product = productService.findByProductName("공책");
+
+        System.out.println(product);
+    }
 }
