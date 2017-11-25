70979027b8d3730dece388a4bccb783a88a316ec
JOHN LEE <nasky987@gmail.com>
2017-11-25 18:57:18 +0900

    DI 기초 2 어노테이션을 이용한 DI
    - 빈 설정파일(xml)에서 context 스키마를 사용한다.
    - annotation-config는 @Autowire나 @Resource 어노테이션을 사용할 때 사용되며,  context의 component-scan 이나, mvc의 annotation-driven 사용시에는 생략이 가능하다.
    - component-scan은  @Component, @Service등의 어노테이션이 설정된 클래스를 읽어 들여 DI 컨테이너에 등록할때 사용한다. base-package 프로퍼티에서 지정한 패키지 아래의 컴포넌트를 검색한다.
    - component-scan의 exclude-filter 프로퍼티를 이용하여 특정 패키지를 제외할 수 있다.
    - use-default-filters 프로퍼티를 false로 지정하고, include-filter프로퍼티를 사용하면 기본 설정은 제외하고, 특정한 패키지를 검색한다?

---
+++ di_example02/src/main/java/sample/di/business/domain/Product.java
@@ -0,0 +1,28 @@
+package sample.di.business.domain;
+
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(final String name, final int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Product{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", price=").append(price);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ di_example02/src/main/java/sample/di/dataaccess/ProductDaoImpl.java
@@ -0,0 +1,23 @@
+package sample.di.dataaccess;
+
+import org.springframework.stereotype.Component;
+import sample.di.business.domain.Product;
+import sample.di.business.service.ProductDao;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class ProductDaoImpl implements ProductDao {
+    private Map<String, Product> storage = new HashMap<>();
+
+    @Override
+    public void addProduct(final Product product) {
+        storage.put(product.getName(), product);
+    }
+
+    @Override
+    public Product findByProductName(final String name) {
+        return storage.get(name);
+    }
+}
